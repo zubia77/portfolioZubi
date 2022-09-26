5601f983681ed2f689a1c5a73413befdc39eb7d0
Zubia <rashzubia@gmail.com>
2022-09-26 09:46:20 +0200

    Navbar

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Works() {
   return (
-    <div className="text-10">Works</div>
+    <div className="text-10 bg-color-20 w-full h-screen -mt-10"></div>
   )
 }
 
